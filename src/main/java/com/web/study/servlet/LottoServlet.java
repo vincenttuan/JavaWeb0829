cbbe489452f39671e884774a99f417dad7600d13
MB-teacher <MB-teacher@192.168.1.157>
2020-08-29 14:57:33 +0800

    JavaWeb

---
+++ src/main/java/com/web/study/servlet/LottoServlet.java
@@ -0,0 +1,35 @@
+package com.web.study.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LottoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // utf-8 編碼
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        
+        // 產出 1 ~ 50 不重複的數字 10 個
+        Random r = new Random();
+        Set<Integer> nums = new LinkedHashSet<>();
+        while(nums.size() < 10) {
+            nums.add(r.nextInt(50) + 1);
+        }
+        
+        // 回應資訊
+        PrintWriter out = resp.getWriter();
+        out.print(nums);
+        
+    }
+    
+}
