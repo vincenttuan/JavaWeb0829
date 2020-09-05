edc76df4664fe7291d855f3b8b8ede1c86351e43
MB-teacher <MB-teacher@192.168.1.157>
2020-09-05 09:51:44 +0800

    JavaWeb

---
+++ src/main/java/com/web/study/servlet/PathServlet.java
@@ -0,0 +1,32 @@
+package com.web.study.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "pathservlet", urlPatterns = {"/servlet/path"})
+public class PathServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        String servletName = req.getServerName();
+        String url = req.getRequestURL().toString();
+        String uri = req.getRequestURI().toString();
+        String contextPath = req.getContextPath();
+        String servletPath = req.getServletPath();
+        String pathInfo = req.getPathInfo();
+        out.println("servletName: " + servletName);
+        out.println("url: " + url);
+        out.println("uri: " + uri);
+        out.println("contextPath: " + contextPath);
+        out.println("servletPath: " + servletPath);
+        out.println("pathInfo: " + pathInfo);
+    }
+    
+}
