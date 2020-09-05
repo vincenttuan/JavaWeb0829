fa57feb51a11b081c8e578d241ad923aefaccad2
MB-teacher <MB-teacher@192.168.1.157>
2020-09-05 14:37:30 +0800

    JavaWeb

--- src/main/java/com/web/study/mvc/bmi/controller/BMIController.java
+++ src/main/java/com/web/study/mvc/bmi/controller/BMIController.java
@@ -1,7 +1,25 @@
 package com.web.study.mvc.bmi.controller;
 
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "bmicontroller", urlPatterns = {"/mvc/bmi"})
 public class BMIController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/mvc/bmi/view/bmi_form.jsp");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("doPost()");
+    }
+
     
 }
