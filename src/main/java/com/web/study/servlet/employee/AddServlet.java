5f04389eced1b15b0b60987cf45f3a36dadb5958
MB-teacher <MB-teacher@192.168.1.157>
2020-09-05 12:00:13 +0800

    JavaWeb

--- src/main/java/com/web/study/servlet/employee/AddServlet.java
+++ src/main/java/com/web/study/servlet/employee/AddServlet.java
@@ -2,6 +2,10 @@ package com.web.study.servlet.employee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "addservlet", urlPatterns = {"/servlet/employee/add"})
 public class AddServlet extends HttpServlet {
-
+    private static List<Map<String, String>> emps = new ArrayList<>();
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //resp.setContentType("text/plain;charset=utf-8");
@@ -21,16 +26,25 @@ public class AddServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain;charset=utf-8");
+        resp.setContentType("text/html;charset=utf-8");
         String empName = req.getParameter("empName");
         String empSalary = req.getParameter("empSalary");
         String empSex = req.getParameter("empSex");
         String empType = req.getParameter("empType");
+        
+        // 1.紀錄/建立 Map
+        Map<String, String> emp = new LinkedHashMap<>();
+        emp.put("empName", empName);
+        emp.put("empSalary", empSalary);
+        emp.put("empSex", empSex);
+        emp.put("empType", empType);
+        
+        // 2.儲存到emps集合中
+        emps.add(emp);
+        
         PrintWriter out = resp.getWriter();
-        out.println("empName: " + empName);
-        out.println("empSalary: " + empSalary);
-        out.println("empSex: " + empSex);
-        out.println("empType: " + empType);
+        out.println(emps);
+        
     }
     
 }
