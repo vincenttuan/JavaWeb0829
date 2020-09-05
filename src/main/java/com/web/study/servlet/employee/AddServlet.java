f774ffc4075be58ca9f1f3e2227b9bf5389ad121
MB-teacher <MB-teacher@192.168.1.157>
2020-09-05 12:16:19 +0800

    JavaWeb

--- src/main/java/com/web/study/servlet/employee/AddServlet.java
+++ src/main/java/com/web/study/servlet/employee/AddServlet.java
@@ -23,6 +23,10 @@ public class AddServlet extends HttpServlet {
         //resp.getWriter().print("此網頁不提供此方法的調用");
         // 重新導向(外部)至 employee_form.jsp
         resp.sendRedirect("/JavaWeb0829/forms/employee_form.jsp");
+        
+        // 重新導向(內部)至 employee_form.jsp
+        //RequestDispatcher rd = getServletContext().getRequestDispatcher("/forms/employee_form.jsp");
+        //rd.forward(req, resp);
     }
 
     @Override
