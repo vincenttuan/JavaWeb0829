27f348056efbd710ca18a66ab8978ab6b44d2769
MB-teacher <MB-teacher@192.168.1.157>
2020-09-05 11:03:30 +0800

    JavaWeb

--- src/main/java/com/web/study/servlet/employee/AddServlet.java
+++ src/main/java/com/web/study/servlet/employee/AddServlet.java
@@ -1,6 +1,7 @@
 package com.web.study.servlet.employee;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,16 @@ public class AddServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("OK");
+        resp.setContentType("text/plain;charset=utf-8");
+        String empName = req.getParameter("empName");
+        String empSalary = req.getParameter("empSalary");
+        String empSex = req.getParameter("empSex");
+        String empType = req.getParameter("empType");
+        PrintWriter out = resp.getWriter();
+        out.println("empName: " + empName);
+        out.println("empSalary: " + empSalary);
+        out.println("empSex: " + empSex);
+        out.println("empType: " + empType);
     }
     
 }
