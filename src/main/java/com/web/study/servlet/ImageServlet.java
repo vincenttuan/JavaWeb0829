3d2bca23590d1b149384f5831a5a06bfda3afea8
MB-teacher <MB-teacher@192.168.1.157>
2020-09-12 12:09:29 +0800

    JavaWeb

---
+++ src/main/java/com/web/study/servlet/ImageServlet.java
@@ -0,0 +1,29 @@
+package com.web.study.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/servlet/image")
+public class ImageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        File file = new File("C:/upload/" + name);
+        if(file.exists()) {
+            resp.setContentType("image/*");
+            resp.setContentLength((int)file.length());
+            Files.copy(file.toPath(), resp.getOutputStream());
+        } else {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.getWriter().print("Image not found!");
+        }
+    }
+    
+}
