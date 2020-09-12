ff4104dd8d4254d30d07bfeafa7cf02cf5f22fd4
MB-teacher <MB-teacher@192.168.1.157>
2020-09-12 14:03:14 +0800

    JavaWeb

--- src/main/java/com/web/study/job/LongJob.java
+++ src/main/java/com/web/study/job/LongJob.java
@@ -15,9 +15,11 @@ public class LongJob implements Runnable {
     public void run() {
         try {
             PrintWriter out = ctx.getResponse().getWriter();
-            Thread.sleep(10000);
-            out.print("很長的工作做完了, 時間: " + new Date() + "<p>");
-            out.flush();
+            for(int i=1;i<=10;i++) {
+                Thread.sleep(1000);
+                out.print(i + ". 很長的工作做完了, 時間: " + new Date() + "<p>");
+                out.flush();
+            }
             ctx.complete(); // 做完了
         } catch (Exception e) {
         }
