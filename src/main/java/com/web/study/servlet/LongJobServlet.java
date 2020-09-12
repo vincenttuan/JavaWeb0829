8b17be227ab462b6791656757fcde06f15ce882a
MB-teacher <MB-teacher@192.168.1.157>
2020-09-12 13:48:35 +0800

    JavaWeb

---
+++ src/main/java/com/web/study/servlet/LongJobServlet.java
@@ -0,0 +1,36 @@
+package com.web.study.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/servlet/longjob")
+public class LongJobServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print("進入 Servlet 的時間: " + new Date() + "<p>");
+        out.flush();
+        
+        // LongJob
+        try {
+            Thread.sleep(10000);
+            out.print("很長的工作做完了, 時間: " + new Date() + "<p>");
+            out.flush();
+        } catch (Exception e) {
+        }
+        
+        out.print("離開 Servlet 的時間: " + new Date() + "<p>");
+        out.flush();
+    }
+    
+}
