c10fbdca13633bb92d98a71ec59ad8e2f8966c71
MB-teacher <MB-teacher@192.168.1.157>
2020-09-12 16:18:48 +0800

    JavaWeb

--- src/main/java/com/web/study/filter/LoginFilter.java
+++ src/main/java/com/web/study/filter/LoginFilter.java
@@ -1,7 +1,10 @@
 package com.web.study.filter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,14 +15,27 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(urlPatterns = "/report/*")
 public class LoginFilter extends HttpFilter {
-
+    private static String[][] users = {{"admin", "1234"}, {"mary", "1111"}, {"jo", "2222"}};
+    
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
         if(req.getParameter("username") != null && 
            req.getParameter("password") != null &&
            req.getParameter("username").trim().length() > 1 &&
            req.getParameter("password").trim().length() > 1) {
-            chain.doFilter(req, resp);
+            // 確認 username & password 使否合法 ?
+            String username = req.getParameter("username");
+            String password = req.getParameter("password");
+            boolean check = Arrays.stream(users)
+                    .filter(user -> user[0].equals(username) && user[1].equals(password))
+                    .findAny()
+                    .isPresent();
+            if(check) {
+                chain.doFilter(req, resp);
+            } else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/forms/login_form.jsp");
+                rd.forward(req, resp);
+            }
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/forms/login_form.jsp");
             rd.forward(req, resp);
