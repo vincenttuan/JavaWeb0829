990d6f590d5e00631d87c33207fb46352ba25187
MB-teacher <MB-teacher@192.168.1.157>
2020-09-12 14:20:33 +0800

    JavaWeb

---
+++ src/main/java/com/web/study/job/ImageListJob.java
@@ -0,0 +1,36 @@
+package com.web.study.job;
+
+import java.io.File;
+import java.io.PrintWriter;
+import javax.servlet.AsyncContext;
+
+public class ImageListJob implements Runnable {
+
+    private AsyncContext ctx;
+
+    public ImageListJob(AsyncContext ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void run() {
+        try {
+            PrintWriter out = ctx.getResponse().getWriter();
+            String path = "C:/upload";
+            File root = new File(path);
+            String[] names = root.list();
+            for (String name : names) {
+                File file = new File(path + "/" + name);
+                if (file.isFile()) {
+                    String imagePath = "/JavaWeb0829/servlet/image?name=" + name;
+                    out.print("<img width='500' src='" + imagePath + "'>");
+                    out.flush();
+                }
+            }
+            ctx.complete();
+        } catch (Exception e) {
+        }
+
+    }
+
+}
---
+++ src/main/java/com/web/study/servlet/ImageListServlet.java
@@ -0,0 +1,24 @@
+package com.web.study.servlet;
+
+import java.io.IOException;
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/servlet/imagelist", asyncSupported = true)
+public class ImageListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
+        
+        AsyncContext ctx = req.startAsync();
+        
+    }
+    
+}
