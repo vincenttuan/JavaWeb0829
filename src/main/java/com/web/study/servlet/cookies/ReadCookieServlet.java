6354439ef49e5bb43abd3a66ae41fb4f3d54d743
MB-teacher <MB-teacher@192.168.1.157>
2020-09-19 10:08:50 +0800

    JavaWeb

--- src/main/java/com/web/study/servlet/cookies/ReadCookieServlet.java
+++ src/main/java/com/web/study/servlet/cookies/ReadCookieServlet.java
@@ -1,5 +1,29 @@
 package com.web.study.servlet.cookies;
 
-public class ReadCookieServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/cookies/read")
+public class ReadCookieServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies(); // 取得 client 傳來的 cookie 資料
+        PrintWriter out = resp.getWriter();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                out.println(cookie.getName() + ":" + cookie.getValue());
+            }
+        } else {
+            out.println("None cookie !");
+        }
+        
+    }
     
 }
