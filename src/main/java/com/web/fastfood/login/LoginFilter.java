033d7f0a11e33973bdae8a3ec597cd1d35716768
MB-teacher <MB-teacher@192.168.1.157>
2020-09-19 16:07:21 +0800

    JavaWeb

--- src/main/java/com/web/fastfood/login/LoginFilter.java
+++ src/main/java/com/web/fastfood/login/LoginFilter.java
@@ -8,15 +8,29 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter("/fastfood/*")
 public class LoginFilter extends HttpFilter {
-
+    private LoginDao dao = new LoginDao();
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        
-        RequestDispatcher rd = getServletContext().getRequestDispatcher("/forms/login_form.jsp");
-        rd.forward(req, res);
+        HttpSession session = req.getSession();
+        // 判斷是否已經登入過 ?
+        if(session.getAttribute("username") != null) {
+            chain.doFilter(req, res);
+        }
+        // 從 login_form 進入
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        boolean check = dao.login(username, password);
+        if(check) {
+            session.setAttribute("username", username);
+            chain.doFilter(req, res);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/forms/login_form.jsp");
+            rd.forward(req, res);
+        }
     }
     
 }
