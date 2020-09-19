da4e79f5d2f8ec6446794215085ec0e0c657917e
MB-teacher <MB-teacher@192.168.1.157>
2020-09-19 10:13:54 +0800

    JavaWeb

--- src/main/java/com/web/study/servlet/cookies/WriteCookieServlet.java
+++ src/main/java/com/web/study/servlet/cookies/WriteCookieServlet.java
@@ -16,9 +16,13 @@ public class WriteCookieServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int salary = new Random().nextInt(100000);
+        int age = new Random().nextInt(40);
         Cookie cookie = new Cookie("salary", salary + ""); // 建立 cookie 物件
-        cookie.setMaxAge(60); // 存續時間(秒)
+        Cookie cookie2 = new Cookie("age", age + ""); // 建立 cookie 物件
+        cookie.setMaxAge(10); // 存續時間(秒)
+        cookie2.setMaxAge(10); // 存續時間(秒)
         resp.addCookie(cookie); // 將 cookie 寫入 client 端
+        resp.addCookie(cookie2); // 將 cookie 寫入 client 端
         PrintWriter out = resp.getWriter();
         out.println("Write cookie ok !");
         
