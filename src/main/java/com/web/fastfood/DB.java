407e7a452fb5330e37ad0b4f6f62239040aed343
MB-teacher <MB-teacher@192.168.1.157>
2020-09-19 11:56:14 +0800

    JavaWeb

---
+++ src/main/java/com/web/fastfood/DB.java
@@ -0,0 +1,29 @@
+package com.web.fastfood;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DB {
+    public static final Map<String, Integer> mainFoods = new LinkedHashMap<>();
+    public static final Map<String, Integer> secondFoods = new LinkedHashMap<>();
+    public static final Map<String, Integer> drinks = new LinkedHashMap<>();
+    static {
+        mainFoods.put("big_mac", 70);
+        mainFoods.put("double_beef", 80);
+        mainFoods.put("fish", 50);
+        mainFoods.put("pounder_cheese", 90);
+        
+        secondFoods.put("apple_pie", 30);
+        secondFoods.put("chicken", 40);
+        secondFoods.put("french_fries", 20);
+        secondFoods.put("french_fries_big", 40);
+        secondFoods.put("hash_browns", 15);
+        secondFoods.put("salad", 35);
+        
+        drinks.put("coca_cola", 20);
+        drinks.put("corn_soup", 35);
+        drinks.put("ice_cream", 25);
+        drinks.put("iced_coffee", 45);
+        drinks.put("milkshake", 40);
+    }
+}
