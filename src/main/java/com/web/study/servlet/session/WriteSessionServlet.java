281a6a50dc51a4543749505eb0414e645c8b06f6
MB-teacher <MB-teacher@192.168.1.157>
2020-09-19 10:32:09 +0800

    JavaWeb

--- src/main/java/com/web/study/servlet/session/WriteSessionServlet.java
+++ src/main/java/com/web/study/servlet/session/WriteSessionServlet.java
@@ -2,6 +2,7 @@ package com.web.study.servlet.session;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +16,12 @@ public class WriteSessionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(true); // 建立新 session
+        // 將敏感的資料存入 session 中
+        int salary = new Random().nextInt(100000);
+        int age = new Random().nextInt(40);
+        session.setAttribute("salary", salary);
+        session.setAttribute("age", age);
+        
         PrintWriter out = resp.getWriter();
         out.println("Session create ok !");
         out.println("Session Id: " + session.getId());
