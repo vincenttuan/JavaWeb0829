40d652e1194f65ea09a376b9e2ff5fdcfef712f8
MB-teacher <MB-teacher@192.168.1.157>
2020-09-19 13:46:47 +0800

    JavaWeb

--- src/main/java/com/web/fastfood/FoodDao.java
+++ src/main/java/com/web/fastfood/FoodDao.java
@@ -1,5 +1,15 @@
 package com.web.fastfood;
 
+import java.util.Map;
+
 public class FoodDao {
-    
+    public Map<String, Integer> getMainFoods() {
+        return DB.mainFoods;
+    }
+    public Map<String, Integer> getSecondFoods() {
+        return DB.secondFoods;
+    }
+    public Map<String, Integer> getDrinks() {
+        return DB.drinks;
+    }
 }
--- src/main/java/com/web/fastfood/OrderServlet.java
+++ src/main/java/com/web/fastfood/OrderServlet.java
@@ -10,9 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/fastfood/order")
 public class OrderServlet extends HttpServlet {
-
+    private FoodDao dao = new FoodDao();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("mainFoods", dao.getMainFoods());
+        req.setAttribute("secondFoods", dao.getSecondFoods());
+        req.setAttribute("drinks", dao.getDrinks());
         RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/mvc/fastfood/order.jsp");
         rd.forward(req, resp);
     }
