c075b7bbbba1ab49c97e3e2a6bd803a0187ff0ac
MB-teacher <MB-teacher@192.168.1.157>
2020-10-03 14:51:34 +0800

    JavaWeb

--- src/main/java/com/web/student/service/StudentService.java
+++ src/main/java/com/web/student/service/StudentService.java
@@ -6,8 +6,10 @@ import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -60,10 +62,54 @@ public class StudentService {
     public Response create(Student student) {
         if(student != null) {
             students.add(student);
-            Message message = new Message(200, "create success !");
+            Message message = new Message(200, "Create success !");
             return Response.ok().entity(message).encoding("utf-8").build();
         } else {
             Message message = new Message(400, "No data (create error) !");
+            return Response.status(message.getCode()).entity(message).encoding("utf-8").build();
+        }
+    }
+    
+    /*
+    uri: /rest/student/1
+    Json 格式
+    {
+        "id": 1,
+        "name": "John",
+        "score": 70
+    }
+    */
+    @Path("{id}")
+    @PUT
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response update(@PathParam("id") Integer id, Student student) {
+        Optional<Student> st = students.stream().filter(s -> s.getId() == id).findFirst();
+        if(st.isPresent()) {
+            st.get().setName(student.getName());
+            st.get().setScore(student.getScore());
+            Message message = new Message(200, "Update success !");
+            return Response.ok().entity(message).encoding("utf-8").build();
+        } else {
+            Message message = new Message(400, "No data (ureate error) !");
+            return Response.status(message.getCode()).entity(message).encoding("utf-8").build();
+        }
+    }
+    
+    /*
+    uri: /rest/student/1
+    */
+    @Path("{id}")
+    @DELETE
+    @Produces("application/json")
+    public Response delete(@PathParam("id") Integer id) {
+        Optional<Student> st = students.stream().filter(s -> s.getId() == id).findFirst();
+        if(st.isPresent()) {
+            students.remove(st.get());
+            Message message = new Message(200, "Delete success !");
+            return Response.ok().entity(message).encoding("utf-8").build();
+        } else {
+            Message message = new Message(400, "No data (delete error) !");
             return Response.status(message.getCode()).entity(message).encoding("utf-8").build();
         }
     }
