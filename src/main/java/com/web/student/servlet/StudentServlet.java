00141ec786235dc78366e2c390171ff70560ecd1
MB-teacher <MB-teacher@192.168.1.157>
2020-10-03 15:40:40 +0800

    JavaWeb

--- src/main/java/com/web/student/servlet/StudentServlet.java
+++ src/main/java/com/web/student/servlet/StudentServlet.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.web.student.servlet;
 
-/**
- *
- * @author MB-teacher
- */
-public class StudentServlet {
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/student")
+public class StudentServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/mvc/student/student.jsp");
+        rd.forward(req, resp);
+    }
     
 }
