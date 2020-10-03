0ffbdcb6ad9763fe77dd58cc4e80bcf8761b0fad
MB-teacher <MB-teacher@192.168.1.157>
2020-10-03 14:33:14 +0800

    JavaWeb

--- src/main/java/com/web/student/service/StudentService.java
+++ src/main/java/com/web/student/service/StudentService.java
@@ -3,9 +3,11 @@ package com.web.student.service;
 import com.web.student.entity.Message;
 import com.web.student.entity.Student;
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -21,6 +23,19 @@ public class StudentService {
             return Response.ok().entity(students).encoding("utf-8").build();
         } else {
             Message message = new Message(400, "No data !");
+            return Response.status(message.getCode()).entity(message).encoding("utf-8").build();
+        }
+    }
+    
+    @Path("{id}")
+    @GET
+    @Produces("application/json")
+    public Response get(@PathParam("id") Integer id) {
+        Optional<Student> st = students.stream().filter(s -> s.getId() == id).findFirst();
+        if(st.isPresent()) {
+            return Response.ok().entity(st.get()).encoding("utf-8").build();
+        } else {
+            Message message = new Message(400, String.format("id: %d not fount", id));
             return Response.status(message.getCode()).entity(message).encoding("utf-8").build();
         }
     }
