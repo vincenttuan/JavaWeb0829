6d1ccc7beebeb81fcc5b7e82fa81378796b5fd7e
MB-teacher <MB-teacher@192.168.1.157>
2020-10-03 10:29:50 +0800

    JavaWeb

--- src/main/java/com/web/study/rest/HelloService.java
+++ src/main/java/com/web/study/rest/HelloService.java
@@ -2,15 +2,26 @@ package com.web.study.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("hello")
 public class HelloService {
     
+    // uri: /rest/hello/john
+    // url: http://localhost:8080/JavaWeb0829/rest/hello/john
     @Path("john")
     @GET
     @Produces("text/plain")
     public String helloJohn() {
         return "Hello John";
+    }
+    
+    // uri: /rest/hello/mary, /rest/hello/jo, /rest/hello/anita ...
+    @Path("{name}")
+    @GET
+    @Produces("text/plain")
+    public String hello(@PathParam("name") String name) {
+        return "Hello " + name;
     }
 }
