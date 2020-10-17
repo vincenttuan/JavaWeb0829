165166811f23ec9547fe93642f80b7cb69703105
MB-teacher <MB-teacher@192.168.1.157>
2020-10-17 10:41:21 +0800

    JavaWeb

---
+++ src/main/java/com/web/captcha/SSOServletContextListener.java
@@ -0,0 +1,21 @@
+package com.web.captcha;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class SSOServletContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("captcha_client_key", "6Lf_JtYZAAAAAEIE8pIOhd_yIwvaV2gTrsf7vhPX");
+        sce.getServletContext().setAttribute("captcha_server_key", "6Lf_JtYZAAAAAKYtDPaFi-d8gWe-M7L3TejxS5Pa");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ServletContextListener.super.contextDestroyed(sce); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
