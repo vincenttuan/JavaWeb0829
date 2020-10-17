13290655686c2015c987d3d2b076a2590e986d9e
MB-teacher <MB-teacher@192.168.1.157>
2020-10-17 15:43:28 +0800

    JavaWeb

---
+++ src/main/java/com/web/captcha/SSOLogoutServlet.java
@@ -0,0 +1,27 @@
+package com.web.captcha;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/captcha/sso/logout")
+public class SSOLogoutServlet extends BaseServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session.getAttribute("username") == null) {
+            req.setAttribute("result", "登出失敗: 無登入資訊");
+        } else {
+            String username = session.getAttribute("username") + "";
+            session.removeAttribute("username");
+            req.setAttribute("result", username + " 已登出");
+        }
+        forward(req, resp, "/forms/captcha/sso_login_form.jsp");
+    }
+    
+}
--- src/main/webapp/forms/captcha/sso_login_form.jsp
+++ src/main/webapp/forms/captcha/sso_login_form.jsp
@@ -23,6 +23,7 @@
                     data-action='submit'>登入</button>
             <button class="pure-button pure-button-primary" type="button" onclick="window.location.href='${pageContext.request.contextPath}/forms/captcha/sso_login_new.jsp'">建立</button>
             <button class="pure-button pure-button-primary" type="button"  onclick="window.location.href='${pageContext.request.contextPath}/captcha/sso/view/member'">檢視</button>
+            <button class="pure-button pure-button-primary" type="button"  onclick="window.location.href='${pageContext.request.contextPath}/captcha/sso/logout'">登出</button>
         </form>
         <font color="red">${result}</font>
     </body>
