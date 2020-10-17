fa83d4355381af5f9a03ae967ff4a2bab36f9277
MB-teacher <MB-teacher@192.168.1.157>
2020-10-17 15:58:17 +0800

    JavaWeb

--- src/main/java/com/web/captcha/SSOServletContextListener.java
+++ src/main/java/com/web/captcha/SSOServletContextListener.java
@@ -1,14 +1,20 @@
 package com.web.captcha;
 
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class SSOServletContextListener implements ServletContextListener {
-
+    
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        Set<String> alreadyLoginMembers = new LinkedHashSet<>();
+        sce.getServletContext().setAttribute("alreadyLoginMembers", alreadyLoginMembers);
+        
         sce.getServletContext().setAttribute("captcha_verify_url", "https://www.google.com/recaptcha/api/siteverify");
         sce.getServletContext().setAttribute("captcha_remoteip", "localhost");
         sce.getServletContext().setAttribute("captcha_client_key", "6Lf_JtYZAAAAAEIE8pIOhd_yIwvaV2gTrsf7vhPX");
