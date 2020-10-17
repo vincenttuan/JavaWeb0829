5edd77f76964e77819bec2f223752d743febd2a7
MB-teacher <MB-teacher@192.168.1.157>
2020-10-17 10:09:15 +0800

    JavaWeb

--- src/main/java/com/web/captcha/SSOLoginServlet.java
+++ src/main/java/com/web/captcha/SSOLoginServlet.java
@@ -1,9 +1,19 @@
 package com.web.captcha;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/captcha/sso/login")
 public class SSOLoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String grr = req.getParameter("g-recaptcha-response");
+        resp.getWriter().print(grr);
+    }
     
 }
