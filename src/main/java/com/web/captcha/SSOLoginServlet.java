1d3a6c6236a8b1ae583440d95edc8189c1b436d4
MB-teacher <MB-teacher@192.168.1.157>
2020-10-17 10:18:46 +0800

    JavaWeb

--- src/main/java/com/web/captcha/SSOLoginServlet.java
+++ src/main/java/com/web/captcha/SSOLoginServlet.java
@@ -1,24 +1,31 @@
 package com.web.captcha;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import jdk.nashorn.api.scripting.URLReader;
 
 @WebServlet("/captcha/sso/login")
 public class SSOLoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
         String grr = req.getParameter("g-recaptcha-response");
         resp.getWriter().print(grr);
+        resp.getWriter().print("<hr>");
         // 驗證 CAPTCHA
         String url = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s&remoteip=%s";
         String secret = "6Lf_JtYZAAAAAKYtDPaFi-d8gWe-M7L3TejxS5Pa";
         String remoteip = "localhost";
         url = String.format(url, secret, grr, remoteip);
+        String json = new Scanner(new URL(url).openStream()).useDelimiter("\\A").next();
+        resp.getWriter().print(json);
     }
     
 }
