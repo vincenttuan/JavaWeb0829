278396e84145f32ef9bc04371c709ae31de3b1af
MB-teacher <MB-teacher@192.168.1.157>
2020-10-17 10:52:11 +0800

    JavaWeb

---
+++ src/main/java/com/web/captcha/BaseServlet.java
@@ -0,0 +1,34 @@
+package com.web.captcha;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Scanner;
+import javax.servlet.http.HttpServlet;
+
+public class BaseServlet extends HttpServlet {
+    protected boolean checkCaptcha(String grr) throws MalformedURLException, IOException {
+        System.out.println(grr);
+        // 取得 CAPTCHA 回應碼
+        String url      = getServletContext().getAttribute("captcha_verify_url") + "?secret=%s&response=%s&remoteip=%s";
+        String secret   = getServletContext().getAttribute("captcha_server_key").toString();
+        String remoteip = getServletContext().getAttribute("captcha_remoteip").toString();
+        url = String.format(url, secret, grr, remoteip);
+        String json = new Scanner(new URL(url).openStream()).useDelimiter("\\A").next();
+        System.out.println(json);
+        // 驗證 CAPTCHA
+        Gson gson = new Gson();
+        // Map<String, Object>
+        Map<String, Object> map = gson.fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+        boolean success = Boolean.parseBoolean(map.get("success").toString());
+        double score = 0;
+        if(success) {
+            score = Double.parseDouble(map.get("score").toString());
+        }
+        boolean check = success && score > 0.7 ? true : false;
+        return check;
+    }
+}
--- src/main/java/com/web/captcha/SSOLoginServlet.java
+++ src/main/java/com/web/captcha/SSOLoginServlet.java
@@ -14,33 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 import jdk.nashorn.api.scripting.URLReader;
 
 @WebServlet("/captcha/sso/login")
-public class SSOLoginServlet extends HttpServlet {
+public class SSOLoginServlet extends BaseServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
         String grr = req.getParameter("g-recaptcha-response");
-        resp.getWriter().print(grr);
-        resp.getWriter().print("<hr>");
-        // 取得 CAPTCHA 回應碼
-        String url      = getServletContext().getAttribute("captcha_verify_url") + "?secret=%s&response=%s&remoteip=%s";
-        String secret   = getServletContext().getAttribute("captcha_server_key").toString();
-        String remoteip = getServletContext().getAttribute("captcha_remoteip").toString();
-        url = String.format(url, secret, grr, remoteip);
-        String json = new Scanner(new URL(url).openStream()).useDelimiter("\\A").next();
-        resp.getWriter().print(json);
-        resp.getWriter().print("<hr>");
-        // 驗證 CAPTCHA
-        Gson gson = new Gson();
-        // Map<String, Object>
-        Map<String, Object> map = gson.fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
-        boolean success = Boolean.parseBoolean(map.get("success").toString());
-        double score = 0;
-        if(success) {
-            score = Double.parseDouble(map.get("score").toString());
-        }
-        boolean check = success && score > 0.7 ? true : false;
-        resp.getWriter().print("Check CAPTCHA: " + check);
+        resp.getWriter().print(checkCaptcha(grr));
     }
     
 }
