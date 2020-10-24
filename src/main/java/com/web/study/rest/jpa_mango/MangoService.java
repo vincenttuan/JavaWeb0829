99e753173460ecc3c2c0c1066d009d4e7b97d754
MB-teacher <MB-teacher@192.168.1.157>
2020-10-24 15:50:08 +0800

    JavaWeb

--- src/main/java/com/web/study/rest/jpa_mango/MangoService.java
+++ src/main/java/com/web/study/rest/jpa_mango/MangoService.java
@@ -1,17 +1,39 @@
 package com.web.study.rest.jpa_mango;
 
+import com.web.student.entity.Message;
+import com.web.student.entity.Student;
+import com.web.study.rest.jpa_mango.entity.Customer;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 @Path("mango")
 public class MangoService {
+    // http://localhost:8080/JavaWeb0829/rest/mango/customers
+    @Path("customers")
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public String create(Customer customer, @Context HttpServletRequest req) {
+        EntityManager em = getEntityManager(req);
+        EntityTransaction et = em.getTransaction();
+        et.begin(); // 交易開始
+        em.persist(customer); // 將 customer 加入
+        et.commit(); // 交易提交
+        em.close();
+        return "OK";
+    }
+    
     // http://localhost:8080/JavaWeb0829/rest/mango/customers
     @Path("customers")
     @GET
