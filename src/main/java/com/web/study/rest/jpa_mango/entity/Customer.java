f80e91ec9b41fa8d70d90ac47edf1865a61073c4
MB-teacher <MB-teacher@192.168.1.157>
2020-10-24 14:29:47 +0800

    JavaWeb

--- src/main/java/com/web/study/rest/jpa_mango/entity/Customer.java
+++ src/main/java/com/web/study/rest/jpa_mango/entity/Customer.java
@@ -7,10 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "customer")
+@NamedQueries({
+    @NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c"),
+})
 public class Customer implements Serializable {
 
     @Id
---
+++ src/main/java/com/web/study/rest/jpa_mango/service/MangoService.java
@@ -0,0 +1,26 @@
+package com.web.study.rest.jpa_mango.service;
+
+import com.web.study.rest.jpa_mango.entity.Customer;
+import com.web.study.rest.lotto.Lotto;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+
+@Path("mango")
+public class MangoService {
+    // uri: /rest/lotto/
+    @Path("/customers")
+    @GET
+    @Produces("application/json")
+    public List readAllCustomer(@Context HttpServletRequest req) {
+        EntityManagerFactory emf = (EntityManagerFactory)req.getServletContext().getAttribute("emf");
+        EntityManager em = emf.createEntityManager();
+        return em.createNamedQuery("Customer.findAll").getResultList();
+    }
+}
