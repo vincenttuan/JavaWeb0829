316b56c8fcf3a7e57979ce3d724c06f15b1f26e5
MB-teacher <MB-teacher@192.168.1.157>
2020-10-31 15:53:27 +0800

    JavaWeb

---
+++ src/main/java/com/web/websocket/HelloServer.java
@@ -0,0 +1,24 @@
+package com.web.websocket;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/websocket/hello")
+public class HelloServer {
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("進入 session: " + session.getId());
+    }
+    @OnMessage
+    public String onMessage(String message, Session session) {
+        System.out.println("message ==> " + session.getId() + " 說: " + message);
+        return "Server 收到: " + message;
+    }
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("關閉/離開 session: " + session.getId());
+    }
+}
