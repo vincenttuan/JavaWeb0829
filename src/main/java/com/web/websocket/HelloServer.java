15cf0057f2d835ee8a64d2e89b6a353b0ac2d0b4
MB-teacher <MB-teacher@192.168.1.157>
2020-10-31 16:19:47 +0800

    JavaWeb

--- src/main/java/com/web/websocket/HelloServer.java
+++ src/main/java/com/web/websocket/HelloServer.java
@@ -1,5 +1,6 @@
 package com.web.websocket;
 
+import java.util.concurrent.CopyOnWriteArraySet;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -8,17 +9,27 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/websocket/hello")
 public class HelloServer {
+    private static CopyOnWriteArraySet<Session> sessions;
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("進入 session: " + session.getId());
+        if(sessions == null) {
+            sessions = new CopyOnWriteArraySet<>();
+        }
+        sessions.add(session);
+        sendAll("歡迎 " + session.getId() + " 加入");
     }
     @OnMessage
-    public String onMessage(String message, Session session) {
+    public void onMessage(String message, Session session) {
         System.out.println("id=" + session.getId() + " 說: " + message);
-        return "id=" + session.getId() + " 說: " + message;
+        sendAll("id=" + session.getId() + " 說: " + message);
     }
     @OnClose
     public void onClose(Session session) {
         System.out.println("關閉/離開 session: " + session.getId());
+        if(sessions != null) {
+            sessions.remove(session);
+            sendAll("哇哩勒~ " + session.getId() + " 離開了!");
+        }
     }
 }
